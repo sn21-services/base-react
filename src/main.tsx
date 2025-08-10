73b54d348e3afecb97872e17e9b37f95fba77945
sonle <sonweb2001@gmail.com>
2025-08-10 22:31:38 +0700

    feat: config axios

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,25 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ConfigProvider } from 'antd-mobile';
 import enUS from 'antd-mobile/es/locales/en-US';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './index.css';
 import App from './App.tsx';
 import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary.tsx';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <ConfigProvider locale={enUS}>
-        <App />
-      </ConfigProvider>
+      <QueryClientProvider client={queryClient}>
+        <ConfigProvider locale={enUS}>
+          <App />
+        </ConfigProvider>
+
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ErrorBoundary>
   </StrictMode>
 );
